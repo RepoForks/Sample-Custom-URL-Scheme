3b26144da98b36c71edb54120696832c5ca55a36
Steve Tsokev <Stefan.Tsokev@telerik.com>
2013-08-05 09:02:24 -0500

    styling, WebIntent plug-in, redirect uri

--- sample-custom-url-scheme/js/index.js
+++ sample-custom-url-scheme/js/index.js
@@ -1,50 +1,56 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
         navigator.splashscreen.hide();
+        if (device.platform == 'Android') {
+            handleURLAndroid();
+        }
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
 };
+
+function handleOpenURL(url) {
+    if (url !== "") {
+        var appPath = window.location.href;
+        var path = appPath.substr(0, appPath.lastIndexOf("/") + 1);
+        var queryString = url.substr(url.lastIndexOf("//") + 1, url.length);
+        var activeurl = path + queryString;
+        document.querySelector("#test").innerHTML = "Invocation URL was: " + url;
+        navigator.notification.alert(
+            'Do you want to navigate?',
+            function(btnIndex) {
+                Navigate.apply(this, [{btnIndex:btnIndex, url:activeurl}])
+            }, // callback
+            null, // title
+            'Go Ahead!'  // buttonName
+            );
+    }
+}
+
+function handleURLAndroid() {
+    window.plugins.webintent.getUri(function(url) {
+        if (url !== "") {
+            var appPath = window.location.href;
+            var path = appPath.substr(0, appPath.lastIndexOf("/") + 1);
+            var queryString = url.substr(url.lastIndexOf("//") + 1, url.length);
+            var activeurl = path + queryString;
+            document.querySelector("#test").innerHTML = "Invocation URL was: " + url;
+            navigator.notification.alert(
+                'Do you want to navigate?',
+                function(btnIndex) {
+                    Navigate.apply(this, [{btnIndex:btnIndex, url:activeurl}])
+                }, // callback
+                null, // title
+                'Go Ahead!'  // buttonName
+                );
+        }
+    });   
+}	
+function Navigate(args) {
+    window.location.href = args.url;
+}
